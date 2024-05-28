a4fd65a09a546320b13d47f2b80b917d323e8846
George Daris <88941705+GeorgeDaris@users.noreply.github.com>
2024-05-28 19:54:16 +0300

    fix #8

--- pages/index.vue
+++ pages/index.vue
@@ -52,8 +52,16 @@ useSeoMeta({
 //     return await queryContent('main/landing-page/about').findOne();
 // })
 
-const data = await queryContent('main/1.landing-page/about').findOne();
+let data = ref();
 
+onMounted(async () => {
+  try {
+    data.value = await queryContent('main/landing-page/about').findOne();
+  } catch (err) {
+    error.value = err;
+    console.error('Content query failed:', err);
+  }
+});
 </script>
 
 <style scoped>
