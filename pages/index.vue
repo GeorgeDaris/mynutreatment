c1555b63d93a8bfadb528995d594a96214b101d0
George Daris <88941705+GeorgeDaris@users.noreply.github.com>
2024-05-28 19:39:56 +0300

    fix #6

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@
       </div>
     </address> -->
 
-    <!-- <AboutCard :data="data" :is-section="true" /> -->
+    <AboutCard :data="data" :is-section="true" />
 
     <!-- <LandingServices />
 
@@ -51,6 +51,8 @@ useSeoMeta({
 // const {data} = await useAsyncData('about', async () => {
 //     return await queryContent('main/landing-page/about').findOne();
 // })
+
+const data = await queryContent('main/landing-page/about').findOne();
 
 </script>
 
